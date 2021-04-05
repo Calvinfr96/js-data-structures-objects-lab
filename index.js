963aab0b1e3ef22b776ed8cc870cc77c5b353066
calvin <calvin.francis96@outlook.com>
2021-04-04 23:43:23 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let driver = {};
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newObj = Object.assign({}, driver, { [key]: value })
+    return newObj
+} //Test won't pass even though function satisfies requirements
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver
+} //Test won't pass even though function satisfies requirements
+
+function deleteFromDriverByKey(driver, key) {
+    const newObj = Object.assign({}, driver)
+    delete newObj[key]
+    return newObj
+}//Test won't pass even though function satisfies requirements 
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}//Test won't pass even though function satisfies requirements
+
